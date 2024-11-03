ba0deb2f0c25eaf9ea7e1e1bd9bbdc42d9f074dd
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2024-11-03 12:59:06 -0800

    Update file 11/3/2024, 12:59:06 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -5,7 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1) :
+        # brutefore approach:
+        """for i in range(len(nums)-1) :
             for j in range(i+1, len(nums)):
                 if nums[i]+nums[j] == target:      
-                    return [i,j]
+                    return [i,j]"""
+
+        # using hashmap dictionary:
+        numToIndexMap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in numToIndexMap:
+                return [numToIndexMap[diff], i]
+            else: 
+                numToIndexMap[nums[i]] = i
+
+        return "null"
+        
