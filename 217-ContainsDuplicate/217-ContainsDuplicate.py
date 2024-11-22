e38c65e0b1c6dba069f5c4c23fe1d3d2ce4ba939
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2024-11-21 19:39:53 -0800

    Update file 11/21/2024, 7:39:53 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,18 +1,10 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        #initialize empty dictionary/hashmap for storing value and index positions
-        resultMap = {}
-        #interate through the loop by index and value for the nums array
-        for index, val in enumerate(nums):
-            #if val appears in resultMap return true
-            if val in resultMap:
+        hset = set()
+        for idx in nums:
+            if idx in hset:
                 return True
             else:
-                #add value and index to resultMap dictionary
-                resultMap[val] = index
-        #if value doesn't appear in the hashmap return false        
-        return False
+                hset.add(idx)
