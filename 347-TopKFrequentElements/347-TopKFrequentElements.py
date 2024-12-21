3150ccdc45276e92bb0bdbc6975e33c884443505
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2024-12-20 22:55:21 -0800

    Update file 12/20/2024, 10:55:21 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,16 +1,24 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        freq = {}
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    
+    # create a hashmap count to store number and it frequencies
+        count = {}
+      # create a empty indices frequency including the last position of the nums
+        freq = [[] for i in range(len(nums)+1)]
+
+        # append count hashmap with num and frequencies
         for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+            count[num] = 1 + count.get(num, 0)
         
-        # Sort dictionary items by value (frequency) in descending order
-        # and get top k elements
-        return [num for num, count in sorted(freq.items(), 
-                                          key=lambda x: x[1], 
-                                          reverse=True)[:k]]
+        # update frequencies array
+        for num, val in count.items():
+            freq[val].append(num)
+
+        # return result by moving from -1 index
+
+        res = []
+        for i in range(len(freq)-1, 0 , -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
