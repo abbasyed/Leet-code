61f5cdf16ffd17c8daf96a5ea1bac3f357e5cc5c
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2025-01-11 20:16:59 -0800

    Create file

---
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        """#bruteforce
+        n = len(nums)
+        res = [0]*n
+
+        for i in range(n):
+            prod = 1
+            for j in range(n):
+                if i==j:
+                    continue
+                prod *= nums[j]
+            res[i] = prod
+        return res"""
+
+        #optimal:
+
+        res = [1]*len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+
+        return res
+        
+
+
+        
