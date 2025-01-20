075b37b859239ebda1344505905c19942689e8d9
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2025-01-20 01:50:27 -0800

    Create file

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+
+        intersection = nums1_set.intersection(nums2_set)
+
+        return list(intersection)
