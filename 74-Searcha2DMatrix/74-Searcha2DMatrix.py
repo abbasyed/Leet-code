0527e0a05c7036b411b205923fe1368de1a232e7
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2025-02-15 10:26:06 -0800

    File updated at 2/15/2025, 10:26:06 AM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.py
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -1,16 +1,9 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        flattened_list = [item for sublist in matrix for item in sublist]
-
-        low = 0
-        high = len(flattened_list) - 1
-        while low <= high:
-            mid = (high + low) // 2
-            if flattened_list[mid] == target:
-                return True
-            elif flattened_list[mid] > target:
-                high = mid - 1
-            else:
-                low = mid+1
+        # brute force
+        for r in range(len(matrix)):
+            for c in range (len(matrix[0])):
+                if matrix[r][c] == target:
+                    return True
         return False
