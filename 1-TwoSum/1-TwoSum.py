59871b2d90dc978ebacc9fe65c20a235d011d715
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2025-03-09 18:19:11 -0700

    File updated at 3/9/2025, 6:19:10 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,24 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # brutefore approach:
-        """for i in range(len(nums)-1) :
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:      
-                    return [i,j]"""
-
-        # using hashmap dictionary:
-        numToIndexMap = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in numToIndexMap:
-                return [numToIndexMap[diff], i]
-            else: 
-                numToIndexMap[nums[i]] = i
-
-        return "null"
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        hashMap = {}
+
+        for i, num in enumerate(nums):
+            diff = target - nums[i]
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[num] = i
+        return [] 
