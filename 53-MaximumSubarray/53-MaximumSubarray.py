5fdfd83d32dae88609c920bc7b57c85c8b505d48
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2025-03-10 00:30:08 -0700

    File updated at 3/10/2025, 12:30:08 AM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        curr_sum = nums[0]
-        max_sum = nums[0]
+        curr_sum = nums[0] #sum ending at the current position
+        max_sum = nums[0] #max_sum found so far
 
         for i in range(1, len(nums)):
             curr_sum = max(nums[i], curr_sum + nums[i])
