0d1affac7702cbf8789bc5decbb8f21754e029e6
Abbas Syed <86834814+abbasyed@users.noreply.github.com>
2025-05-07 23:41:41 -0700

    File updated at 5/7/2025, 11:41:41 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,17 +1,13 @@
-// Last updated: 5/7/2025, 11:27:49 PM
+// Last updated: 5/7/2025, 11:41:41 PM
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int [] res = new int[nums.length];
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> res= new HashSet<>();
 
-        int prefix = 1;
-        for (int i=0; i<nums.length; i++){
-            res[i] = prefix;
-            prefix *= nums[i];
-        }
-        int suffix = 1;
-        for (int i = nums.length -1; i >= 0; i--){
-            res[i] *= suffix;
-            suffix *= nums[i];
-        }return res;
+        for(int num: nums){
+            if(res.contains(num)){
+                return true;
+            }
+            res.add(num);
+        }return false;
     }
 }
