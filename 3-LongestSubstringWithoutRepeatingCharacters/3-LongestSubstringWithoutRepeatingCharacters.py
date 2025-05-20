ce314a84dc500b43454ea85592ff0688e86d0097
Abbas Syed <86834814+abbasyed@users.noreply.github.com>
2025-05-20 15:32:18 -0700

    File updated at 5/20/2025, 3:32:17 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,14 +1,19 @@
-# Last updated: 5/20/2025, 3:19:24 PM
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
-        len1 = len(nums1)
-        len2 = len(nums2)
-        merged = nums1 + nums2
-        merged.sort()
+# Last updated: 5/20/2025, 3:32:17 PM
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        res = nums1+nums2
+        res.sort()
 
-        totalLen = len1 + len2
-        if totalLen % 2 == 0:
-            return (merged[totalLen // 2 - 1] + merged[totalLen // 2]) / 2.0
+        length = len(nums1)+len(nums2)
+        if length % 2 == 0:
+            return (res[length // 2 - 1] + res[length // 2]) / 2.0
+            
         else:
-            return merged[totalLen // 2]
+            return res[length // 2]
+
+            
