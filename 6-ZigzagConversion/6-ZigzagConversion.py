0bdb3fe017901ff749aadd38989f8b48d63b0416
Abbas Syed <86834814+abbasyed@users.noreply.github.com>
2025-07-15 18:42:03 -0700

    File updated at 7/15/2025, 6:42:03 PM

--- 6-ZigzagConversion/6-ZigzagConversion.py
+++ 6-ZigzagConversion/6-ZigzagConversion.py
@@ -1,25 +1,2 @@
-# Last updated: 7/15/2025, 6:41:13 PM
-class Solution(object):
-    def resultsArray(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        result = []
-
-        for i in range(len(nums) - k + 1):
-            window = nums[i:i+k]
-        
-        # Check if sorted in ascending order
-            is_sorted = all(window[j] < window[j+1] for j in range(k - 1))
-        
-        # Check if all elements are consecutive
-            is_consecutive = max(window) - min(window) == k - 1 and len(set(window)) == k
-        
-            if is_sorted and is_consecutive:
-                result.append(max(window))
-            else:
-                result.append(-1)
-    
-        return result
+# Last updated: 7/15/2025, 6:42:03 PM
+return df.rename(columns={'experience_years': 'average_years'}).round(2)
