fc0d2a4b4caa1f7e65978d54665d2cc1a2951ecd
Abbas Syed <86834814+abbasyed@users.noreply.github.com>
2025-09-14 04:58:47 -0700

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 9/14/2025, 4:58:46 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
