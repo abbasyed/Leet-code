9abdcfdeca327778725efe31dc0c53c67c24bc4f
Abbas Syed <86834814+abbasyed@users.noreply.github.com>
2025-09-14 04:59:12 -0700

    Create 138-CopyListWithRandomPointer

---
+++ 138-CopyListWithRandomPointer/138-CopyListWithRandomPointer.py
@@ -0,0 +1,36 @@
+# Last updated: 9/14/2025, 4:59:12 AM
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # if empty return None
+        if not head:
+            return None
+
+        toCopy = {}
+
+        # copy all the Nodes to toCopy{} hashmap
+        current = head
+        while current:
+            toCopy[current] = Node(current.val)
+            current = current.next
+        
+        # connect Next and Random Pointers
+        current = head
+        while current:
+            if current.next:
+                toCopy[current].next = toCopy[current.next]
+            
+            if current.random:
+                toCopy[current].random = toCopy[current.random]
+
+            current = current.next
+        
+        return toCopy[head]
